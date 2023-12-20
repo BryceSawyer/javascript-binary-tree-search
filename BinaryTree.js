842961068df25ef8a4d7516860675a47a4b738b2
brycesawyer <bryce.sawyer666@gmail.com>
2023-12-20 10:14:54 +1000

    first commit

---
+++ BinaryTree.js
@@ -0,0 +1,50 @@
+// BINARY SEARCH TREES: JAVASCRIPT
+
+class BinaryTree {
+    constructor(value, depth = 1) {
+        this.value = value;
+        this.depth = depth;
+        this.left = null;
+        this.right = null;
+    }
+
+    insert(value) {
+        if (value < this.value) {
+            if (!this.left) {
+                this.left = new BinaryTree(value, this.depth + 1);
+            } else {
+                this.left.insert(value);
+            }
+        } else {
+            if (!this.right) {
+                this.right = new BinaryTree(value, this.depth + 1);
+            } else {
+                this.right.insert(value);
+            }
+        }
+    }
+
+    getNodeByData(value) {
+        if (value === this.value) {
+            return this;
+        } else if (this.left && this.left < value) {
+            this.left.getNodeByData(value);
+        } else if (this.right) {
+            this.right.getNodeByData(value);
+        } else {
+            return null;
+        }
+    }
+
+    depthFirstTraversal() {
+        if (this.left) {
+            this.left.depthFirstTraversal();
+            console.log(`Depth=${this.depth}, Value=${this.value}`);
+        }
+        if (this.right) {
+            this.right.depthFirstTraversal();
+        }
+    }
+}
+
+module.exports = BinaryTree;
